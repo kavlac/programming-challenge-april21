f2b3b938fe68d5992005d2a5821107859795b895
Marco Antonio Cavalcante de Sousa <marco.antonio@icomp.ufam.edu.br>
2021-05-18 19:03:05 -0400

    edit database credentials

--- data_processing/db.js
+++ data_processing/db.js
@@ -1,4 +1,12 @@
 
 const exporter = require('./csv-to-mysql');
 
-exporter('localhost','teste','root','marco');
+const db = { /* don't expose password or any sensitive info, done only for demo */
+      host: 'localhost',
+      user: 'root',
+      password: 'marco',
+      database: 'teste',
+    };
+ 
+
+exporter(db.host,db.database,db.user,db.password);
